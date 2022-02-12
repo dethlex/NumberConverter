e0715db6c81394fab99395a572e5130c39690229
lexis <lexis@trafficstars.com>
2022-02-12 14:51:59 +0200

    Closes #4. Support multi carets and shortcuts

--- src/com/dethlex/numberconverter/ConvertAction.java
+++ src/com/dethlex/numberconverter/ConvertAction.java
@@ -4,33 +4,34 @@ import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.command.WriteCommandAction;
-import com.intellij.openapi.editor.Caret;
-import com.intellij.openapi.editor.CaretModel;
-import com.intellij.openapi.editor.Document;
-import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.editor.*;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.Pair;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 enum ConvertType {
-	HEX{
+	HEX {
 		@Override
 		public String toString() {
 			return "HEX";
 		}
 	},
-	BIN{
+	BIN {
 		@Override
 		public String toString() {
 			return "BIN";
 		}
 	},
-	OCT{
+	OCT {
 		@Override
 		public String toString() {
 			return "OCT";
 		}
 	},
-	DEC{
+	DEC {
 		@Override
 		public String toString() {
 			return "DEC";
@@ -42,8 +43,8 @@ public class ConvertAction extends AnAction {
 
 	private final ConvertType type_;
 
-	private final static String ERR_UT = "Unknown type";
-	private final static String ERR_CC = "Can't convert";
+	private final static String ERR_UT = "unknown type";
+	private final static String ERR_CC = "can't convert";
 
 	public ConvertAction(ConvertType type) {
 		super();
@@ -67,53 +68,87 @@ public class ConvertAction extends AnAction {
 				number = Integer.decode(value);
 
 			switch (type_) {
-				case DEC: value = Integer.toString(number); break;
-				case HEX: value = "0x"+Integer.toHexString(number).toUpperCase(); break;
-				case OCT: value = "0"+Integer.toOctalString(number); break;
-				case BIN: value = "0b"+Integer.toBinaryString(number); break;
-				default: value = ERR_UT;
+				case DEC:
+					value = Integer.toString(number);
+					break;
+				case HEX:
+					value = "0x" + Integer.toHexString(number).toUpperCase();
+					break;
+				case OCT:
+					value = "0" + Integer.toOctalString(number);
+					break;
+				case BIN:
+					value = "0b" + Integer.toBinaryString(number);
+					break;
+				default:
+					value = ERR_UT;
 			}
 		} catch (NumberFormatException e) {
 			return ERR_CC;
 		}
-		return negative ? "-"+value : value;
+		return negative ? "-" + value : value;
+	}
+
+	private Pair<Integer,String> ConvertAll(@NotNull List<Caret> caretList) {
+		int count = 0;
+		String value = "";
+
+		for (Caret caret : caretList){
+			value = ConvertNumber(caret.getSelectedText());
+			if (value.equals(ERR_CC)) {
+				return new Pair<>(0, ERR_CC);
+			}
+			count++;
+		}
+
+		return new Pair<>(count, value);
+	}
+
+	private List<Caret> FilterCaretWithSelection(@NotNull List<Caret> caretList) {
+		return caretList.stream().filter(Caret::hasSelection).collect(Collectors.toList());
 	}
 
 	@Override
 	public void actionPerformed(@NotNull AnActionEvent anActionEvent) {
 		final Editor editor = anActionEvent.getRequiredData(CommonDataKeys.EDITOR);
 		CaretModel caretModel = editor.getCaretModel();
-		Caret currentCaret = caretModel.getCurrentCaret();
+		List<Caret> caretList = FilterCaretWithSelection(caretModel.getAllCarets());
 
-		if (currentCaret.hasSelection()) {
+		for (Caret caret : caretList) {
 			final Project project = anActionEvent.getRequiredData(CommonDataKeys.PROJECT);
 			final Document document = editor.getDocument();
 
-			Caret primaryCaret = caretModel.getPrimaryCaret();
-			int selectionStart = primaryCaret.getSelectionStart();
-			int selectionEnd = primaryCaret.getSelectionEnd();
+			int selectionStart = caret.getSelectionStart();
+			int selectionEnd = caret.getSelectionEnd();
 
-			CharSequence convertedNumber = ConvertNumber(currentCaret.getSelectedText());
+			CharSequence convertedNumber = ConvertNumber(caret.getSelectedText());
 			WriteCommandAction.runWriteCommandAction(project, () ->
-				document.replaceString(selectionStart, selectionEnd, convertedNumber)
+					document.replaceString(selectionStart, selectionEnd, convertedNumber)
 			);
 
-			primaryCaret.removeSelection();
+			caret.removeSelection();
+			caret.moveToOffset(selectionStart);
 		}
 	}
 
 	@Override
-	public void update(AnActionEvent anActionEvent)
-	{
+	public void update(@NotNull AnActionEvent anActionEvent) {
 		final Editor editor = anActionEvent.getRequiredData(CommonDataKeys.EDITOR);
-		Caret currentCaret = editor.getCaretModel().getCurrentCaret();
+		List<Caret> caretList = FilterCaretWithSelection(editor.getCaretModel().getAllCarets());
 
-		anActionEvent.getPresentation().setEnabledAndVisible(currentCaret.hasSelection());
+		anActionEvent.getPresentation().setEnabledAndVisible(!caretList.isEmpty());
+		if (!anActionEvent.getPresentation().isVisible())
+			return;
 
-		if (anActionEvent.getPresentation().isVisible()) {
-			String value = ConvertNumber(currentCaret.getSelectedText());
-			anActionEvent.getPresentation().setText(type_.toString()+": "+value);
-			anActionEvent.getPresentation().setEnabled(!value.equals(ERR_CC));
+		Pair<Integer,String> p = ConvertAll(caretList);
+		String text = type_.toString();
+		if (p.first > 1) {
+			text += " ("+ p.first +")";
+		} else {
+			text += ": " + p.second;
 		}
+
+		anActionEvent.getPresentation().setText(text);
+		anActionEvent.getPresentation().setEnabled(p.first > 0);
 	}
 }
--- src/com/dethlex/numberconverter/ConvertActionGroup.java
+++ src/com/dethlex/numberconverter/ConvertActionGroup.java
@@ -6,14 +6,38 @@ import org.jetbrains.annotations.Nullable;
 
 public class ConvertActionGroup extends ActionGroup {
 
+	public static class Dec extends ConvertAction {
+		public Dec() {
+			super(ConvertType.DEC);
+		}
+	}
+
+	public static class Hex extends ConvertAction {
+		public Hex() {
+			super(ConvertType.HEX);
+		}
+	}
+
+	public static class Oct extends ConvertAction {
+		public Oct() {
+			super(ConvertType.OCT);
+		}
+	}
+
+	public static class Bin extends ConvertAction {
+		public Bin() {
+			super(ConvertType.BIN);
+		}
+	}
+
 	@NotNull
 	@Override
 	public AnAction[] getChildren(@Nullable AnActionEvent anActionEvent) {
 		return new AnAction[]{
-			new ConvertAction(ConvertType.DEC),
-			new ConvertAction(ConvertType.HEX),
-			new ConvertAction(ConvertType.OCT),
-			new ConvertAction(ConvertType.BIN),
+			new Dec(),
+			new Hex(),
+			new Oct(),
+			new Bin(),
 		};
 	}
 }
